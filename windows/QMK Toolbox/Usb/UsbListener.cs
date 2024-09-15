bd34646120c42c5797ea90e99f95e2cff6707206
Dimitris Mantzouranis <d3xter93@gmail.com>
2024-09-15 15:43:04 +0300

    [windows] sonixflasher 2.0.7

--- windows/QMK Toolbox/Usb/UsbListener.cs
+++ windows/QMK Toolbox/Usb/UsbListener.cs
@@ -180,6 +180,8 @@ namespace QMK_Toolbox.Usb
                     return new UsbTinyIspDevice(usbDevice);
                 case BootloaderType.Wb32Dfu:
                     return new Wb32DfuDevice(usbDevice);
+                case BootloaderType.Sn32Dfu:
+                    return new Sn32DfuDevice(usbDevice);
                 default:
                     break;
             }
@@ -313,6 +315,17 @@ namespace QMK_Toolbox.Usb
                     if (productId == 0xDFA0)
                     {
                         return BootloaderType.Wb32Dfu;
+                    }
+                    break;
+                case 0x0c45: // Sonix
+                    switch (productId)
+                    {
+                        case 0x7010: // SN32F260
+                            return BootloaderType.Sn32Dfu;
+                        case 0x7040: // SN32F240b
+                            return BootloaderType.Sn32Dfu;
+                        case 0x7900: // SN32F240
+                            return BootloaderType.Sn32Dfu;
                     }
                     break;
             }
